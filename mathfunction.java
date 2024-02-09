5b16678d41461b2299cfd9c3e770b33a5428862d
sumakubas <sumakubasad66@gmail.com>
2024-02-09 14:20:21 +0530

    program for math function

---
+++ mathfunction.java
@@ -0,0 +1,12 @@
+public class mathfunction {
+    public static void main(String args[]) {
+        System.out.println("maximum is:" + Math.max(6, 7));
+        System.out.println("maximum is:" + Math.min(6, 2));
+        System.out.println("maximum is:" + Math.sqrt(5));
+        System.out.println("maximum is:" + Math.cbrt(7));
+        System.out.println("maximum is:" + Math.random());
+        System.out.println("Absolute is:" + Math.abs(2.7));
+
+    }
+
+}
